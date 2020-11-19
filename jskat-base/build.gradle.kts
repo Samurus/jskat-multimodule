1c64affca5451f414987868a6851a9dca168828d
b0n541 <j@nschaefer.net>
2020-11-19 22:03:36 +0100

    Enable Kotlin for jskat-base

--- jskat-base/build.gradle.kts
+++ jskat-base/build.gradle.kts
@@ -1,3 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation("ch.qos.logback:logback-core:1.2.3")
     implementation("ch.qos.logback:logback-classic:1.2.3")
@@ -5,4 +15,15 @@ dependencies {
     implementation("org.reflections:reflections:0.9.12")
     implementation("com.google.guava:guava:30.0-jre")
     implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "11"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "11"
 }
